e033418a8cb1ae5b3da7048341f749e96744cbed
Babajide Fowotade <jide.b.tade@gmail.com>
2021-09-10 04:59:27 +0100

    chore: move dev dep types to deps

---
+++ src/component/FrequentlyAccessed.tsx
@@ -0,0 +1,9 @@
+const FrequentlyAccessed = () => {
+  return (
+    <ul>
+      <li>Hello</li>
+    </ul>
+  );
+};
+
+export default FrequentlyAccessed;
--- src/module/Home.tsx
+++ src/module/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import FrequentlyAccessed from "../component/FrequentlyAccessed";
 import UrlGeneratorForm from "../component/UrlGeneratorForm";
 
 function Home() {
   return (
     <div>
       <UrlGeneratorForm />
+      <FrequentlyAccessed />
     </div>
   );
 }
